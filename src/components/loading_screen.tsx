31095a42d0994c4c7740b8b3c24b4cfec7d03723
정도헌 <89259535+bslkuye@users.noreply.github.com>
2024-07-08 17:41:16 +0900

    Feat : 로딩화면 구현, 설정

--- src/components/loading_screen.tsx
+++ src/components/loading_screen.tsx
@@ -1,27 +1,22 @@
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 
 const LoadingScreen: React.FC = () => {
-  return <LoadingText>Loading...</LoadingText>
+  return <LoadingContainer></LoadingContainer>
 }
 
 export default LoadingScreen
 
-const loadingAnimation = keyframes`
-  0% { opacity: 0.5; }
-  50% { opacity: 1; }
-  100% { opacity: 0.5; }
-`
-
-const LoadingText = styled.div`
+const LoadingContainer = styled.div`
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   color: white;
-  background-color: black;
+  background-image: url('/obj33.png');
+  background-size: cover;
+  width: 100px;
+  height: 100px;
   padding: 20px;
   border-radius: 10px;
-  font-size: 1.5em;
   text-align: center;
-  animation: ${loadingAnimation} 2s linear infinite;
 `
--- src/view/space.tsx
+++ src/view/space.tsx
@@ -1,11 +1,13 @@
 import Map from '../components/space_background'
 import Object from '../components/object'
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import LoadingScreen from '../components/loading_screen'
 import Menu from '../components/menu'
+import { useRecoilState } from 'recoil'
+import { loading } from '../constants/store'
 
 const Space = () => {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useRecoilState(loading)
 
   useEffect(() => {
     setIsLoading(false)
